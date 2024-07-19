d59802686d14b8f7aa6d60b9f3181871396a5c5f
Rodney Kimathi <rodneykimathi@gmail.com>
2024-07-19 19:23:48 +0300

    Update user setup

--- app/models/user_models.py
+++ app/models/user_models.py
@@ -7,7 +7,7 @@ from app.models.base_models import Base, BaseResponse
 class UserBase(SQLModel):
     username: str = Field(min_length=4, max_length=16, unique=True, nullable=False)
     password: str = Field(min_length=8, max_length=64, nullable=False)
-    email: EmailStr = Field(unique=True, nullable=False)
+    email: EmailStr = Field(min_length=6, max_length=32, unique=True, nullable=False)
     full_name: str = Field(min_length=6, max_length=32, nullable=False)
 
 
